f2156f86115f76db101f3270f9237c6acf4ee642
Lyoko <519052532@qq.com>
2022-09-28 09:56:05 +0800

    feat(types): @yusui/types

---
+++ packages/types/src/helpers.ts
@@ -0,0 +1,5 @@
+export type Data<T = any> = Record<string, T>;
+
+export type TreeNode<T = Data> = T & { children?: TreeNode<T>[] };
+
+export type ElSize = "" | "default" | "small" | "large";
---
+++ packages/types/src/request.ts
@@ -0,0 +1,18 @@
+export interface Page {
+  current?: number;
+  size?: number;
+  pageNum?: number;
+  pageSize?: number;
+  ascs?: string;
+  descs?: string;
+  order?: string;
+  prop?: string;
+}
+
+export type Res<T = any> = { code: number; msg: string } & T;
+
+export type ResData<T = any> = Res<{ data: T }>;
+
+export type ResRows<T = any> = Res<{ rows: T; total: number }>;
+
+export type ResRecords<T = any> = ResData<{ records: T; current: number; pages: number; size: number; total: number }>;
