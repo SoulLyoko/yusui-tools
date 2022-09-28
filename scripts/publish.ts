9f88456def3c4c5962943126845bd17dc1b007f5
Lyoko <519052532@qq.com>
2022-09-28 16:48:24 +0800

    chore: update scripts

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,15 +1,22 @@
+import { exec, execSync } from "child_process";
 import path from "path";
 import npmPublish from "@jsdevtools/npm-publish";
 
 const packages = ["composables", "types", "utils"];
 
-function publish() {
-  packages.forEach(name => {
-    npmPublish({
-      package: path.resolve("packages", name, "package.json"),
-      token: process.env.NPM_TOKEN,
-      access: "public"
-    });
-  });
-}
-publish();
+execSync(`echo "//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}" > .npmrc`);
+execSync(`pnpm publish -r`);
+
+// function publish() {
+// packages.forEach(name => {
+// execSync(`echo "//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}" > .npmrc`, {
+//   cwd: path.join("packages", name)
+// });
+// npmPublish({
+//   package: path.resolve("packages", name, "package.json"),
+//   token: process.env.NPM_TOKEN,
+//   access: "public"
+// });
+//   });
+// }
+// publish();
