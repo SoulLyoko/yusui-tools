d82fc9723f997ad1bb0a632da5312807d9f9b9de
Lyoko <519052532@qq.com>
2022-09-28 10:38:50 +0800

    chore: update scripts

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,11 +1,18 @@
-import { execSync } from "child_process";
-import path from "path";
+// import { execSync } from "child_process";
+// import path from "path";
+import npmPublish from "@jsdevtools/npm-publish";
 
 const packages = ["composables", "types", "utils"];
 
 packages.forEach(name => {
-  execSync("npm publish --access public", {
-    stdio: "inherit",
-    cwd: path.join("packages", name)
+  console.log("🚀 ~ file: publish.ts ~ line 11 ~ process.env", process.env);
+  npmPublish({
+    package: `./packages/${name}/package.json`,
+    token: process.env.NPM_TOKEN,
+    access: "public"
   });
+  // execSync("npm publish --access public", {
+  //   stdio: "inherit",
+  //   cwd: path.join("packages", name)
+  // });
 });
