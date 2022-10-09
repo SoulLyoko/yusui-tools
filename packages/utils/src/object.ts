e3897b91a62c9ad76b729c53c7fceadccde00516
Lyoko <519052532@qq.com>
2022-10-09 16:11:17 +0800

    fix(utils): filterObj return type

--- packages/utils/src/object.ts
+++ packages/utils/src/object.ts
@@ -1,13 +1,11 @@
-import type { Data } from "@yusui/types";
-
 /**
  * 过滤对象值和字段
  * @param {Object} obj 被过滤对象
  * @param {Array} values 被过滤的值
  * @param {Array} fields 被过滤字段
  */
-export function filterObj(obj: Data, values: any[] = [undefined, null], fields: string[] = []) {
-  const temp: Data = {};
+export function filterObj<R = any, T = any>(obj: T, values: any[] = [undefined, null], fields: string[] = []): R {
+  const temp: any = {};
   for (const key in obj) {
     const valuesValid = values.every(val => obj[key] !== val);
     const fieldsValid = fields.every(str => !key.includes(str));
@@ -15,7 +13,7 @@ export function filterObj(obj: Data, values: any[] = [undefined, null], fields: 
       temp[key] = obj[key];
     }
   }
-  return temp;
+  return temp as R;
 }
 
 /** 获取数据类型 */
