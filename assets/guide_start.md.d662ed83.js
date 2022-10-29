8370703475e56984adcf4c3fb5289836396b66dd
SoulLyoko <48344443+SoulLyoko@users.noreply.github.com>
2022-10-29 14:59:49 +0000

    Deploying to gh-pages from @ SoulLyoko/yusui-tools@338737e80f48cd222471b23b33e9f91ad92cba7d 🚀

---
+++ assets/guide_start.md.d662ed83.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c as o,e as r,f as n}from"./app.dd68700d.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/start.md"}'),s={name:"guide/start.md"};function c(d,p,m,_,i,l){const e=n("markdown");return a(),o("div",null,[r(e,{path:"../README.md"})])}const h=t(s,[["render",c]]);export{u as __pageData,h as default};
