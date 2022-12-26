6b29d70aad5cceae7762e482a7c6dbaef771507d
Lyoko <519052532@qq.com>
2022-12-26 14:57:49 +0800

    chore: fix docs

--- packages/.vitepress/components/index.ts
+++ packages/.vitepress/components/index.ts
@@ -1,8 +1,8 @@
-import type { App } from "vue-demi";
+import type { App } from "vue";
 
 export default {
   install: (app: App) => {
-    const components = import.meta.globEager("./**/*.vue");
+    const components = import.meta.glob("./**/*.vue", { eager: true });
     Object.values(components).forEach((module: any) => {
       const component = module.default;
       app.component(component.name, component);
