7097a3c5af104c49ab3fbd891882e9500bfebe0f
Lyoko <519052532@qq.com>
2023-08-29 10:03:02 +0800

    fix(utils): 修复filterObjDeep传入类型不为对象类型的问题

--- packages/utils/object/index.ts
+++ packages/utils/object/index.ts
@@ -5,6 +5,8 @@
  * @param {Array} fields 被过滤字段
  */
 export function filterObj<R = any, T = any>(obj: T, values: any[] = [undefined, null], fields: string[] = []): R {
+  if (typeof obj !== 'object')
+    return obj as any
   const temp: any = {}
   for (const key in obj) {
     const valuesValid = values.every(val => obj[key] !== val)
@@ -22,24 +24,23 @@ export function filterObj<R = any, T = any>(obj: T, values: any[] = [undefined, 
  * @param {Array} fields 被过滤字段
  */
 export function filterObjDeep<R = any, T = any>(obj: T, values: any[] = [undefined, null], fields: string[] = []): R {
-  if (Array.isArray(obj)) {
+  if (typeof obj !== 'object')
+    return obj as any
+  if (Array.isArray(obj))
     return obj.map(e => filterObjDeep(e, values, fields)) as R
-  }
-  else {
-    const temp: any = {}
-    for (const key in obj) {
-      if (typeof obj[key] === 'object') {
-        temp[key] = filterObjDeep(obj[key], values, fields)
-      }
-      else {
-        const valuesValid = values.every(val => obj[key] !== val)
-        const fieldsValid = fields.every(str => !key.includes(str))
-        if (valuesValid && fieldsValid)
-          temp[key] = obj[key]
-      }
+  const temp: any = {}
+  for (const key in obj) {
+    if (typeof obj[key] === 'object') {
+      temp[key] = filterObjDeep(obj[key], values, fields)
     }
-    return temp as R
+    else {
+      const valuesValid = values.every(val => obj[key] !== val)
+      const fieldsValid = fields.every(str => !key.includes(str))
+      if (valuesValid && fieldsValid)
+        temp[key] = obj[key]
+    }
   }
+  return temp as R
 }
 
 /** 获取数据类型 */
