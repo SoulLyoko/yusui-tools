cf22ebaaa3314f97555f5b600791d06cf5fff4ce
Lyoko <519052532@qq.com>
2023-10-19 14:45:53 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'taze'
 
 export default defineConfig({
   // ignore packages from bumping
-  exclude: ['@antfu/eslint-config', '@vueuse/core', 'element-plus', 'vitepress'],
+  exclude: ['@antfu/eslint-config', '@vueuse/core', 'vitepress'],
 })
