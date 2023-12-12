6b73b409715406b5e384ac28941c4cd1e31afb1f
SoulLyoko <48344443+SoulLyoko@users.noreply.github.com>
2023-12-12 05:24:05 +0000

    Deploying to gh-pages from @ SoulLyoko/yusui-tools@23f8c4af5e6c38b79d9749950e1c72b774bc2417 🚀

--- assets/chunks/theme.Bs1-HnZr.js
+++
@@ -1,1 +0,0 @@
-const e={rect:{radius:5,stroke:"black"},circle:{r:18,stroke:"black"},polygon:{stroke:"black"},polyline:{stroke:"black",hoverStroke:"black",selectedStroke:"black"},edgeText:{color:"black"},nodeText:{}};export{e as d};
