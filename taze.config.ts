ab922176a16c277af2d2f3b98b2b6cb9ed024b4f
Lyoko <519052532@qq.com>
2024-05-07 11:07:35 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'taze'
 
 export default defineConfig({
   // ignore packages from bumping
-  exclude: ['@dcloudio/*', 'eslint', 'gulp', 'vite-plugin-mock', 'vue-tsc'],
+  exclude: ['@dcloudio/*', 'element-plus', 'eslint', 'gulp', 'vue-tsc'],
 })
