c9d8dae108403d8cc970c95273c2ebc9c3b970d2
Lyoko <519052532@qq.com>
2024-05-07 17:57:26 +0800

    chore: remove test for fixMenuBtnSlot

--- packages/plugins/avue-patch/index.test.ts
+++ packages/plugins/avue-patch/index.test.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra'
 
-import { fixFormRow, fixMenuBtnSlot, fixMenuBtnType } from '.'
+import { fixFormRow, fixMenuBtnType } from '.'
 
 describe('avuePatch', () => {
   const avue_min_js = fs.readFileSync('node_modules/@smallwei/avue/lib/avue.min.js', 'utf-8')
@@ -11,9 +11,5 @@ describe('avuePatch', () => {
 
   it('fixFormRow ', () => {
     expect(fixFormRow(avue_min_js)).toMatch('tableData:{row:e.form}')
-  })
-
-  it('fixMenuBtnSlot ', () => {
-    expect(fixMenuBtnSlot(avue_min_js)).toMatch('e.$slots,"menu-btn"')
   })
 })
