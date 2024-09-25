59c23c6eaf7c8d7be5af2899a7e99993f2618db9
Lyoko <519052532@qq.com>
2024-09-25 09:25:52 +0800

    fix(uvue): form-item-default显示year-month类型

--- packages/uvue/components/uvue-form/components/form-item-default.vue
+++ packages/uvue/components/uvue-form/components/form-item-default.vue
@@ -4,7 +4,7 @@ import { useAttrs } from 'vue'
 const emit = defineEmits(['update:modelValue'])
 
 const inputTypes = ['input', 'text', 'password', 'number'] // 显示input组件的类型
-const pickerTypes = ['date', 'time', 'datetime'] // 显示picker组件的类型
+const pickerTypes = ['date', 'time', 'datetime', 'year-month'] // 显示picker组件的类型
 const dicTypes = ['select', 'cascader', 'checkbox', 'radio', 'switch'] // 显示picker组件的类型
 
 const attrs: Record<string, any> = useAttrs()
